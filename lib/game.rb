69f5d686b2a9d91fb108c927553c24e188d3546a
Jane Challenge <110110727+janechallenge@users.noreply.github.com>
2022-07-31 21:23:20 +0200

    implicit returns

--- lib/game.rb
+++ lib/game.rb
@@ -13,15 +13,15 @@ class Game
     calculation = @score_1.to_i - @score_2.to_i
 
     if calculation > 0
-      return { @team_1 => 3, @team_2 => 0 }
+      { @team_1 => 3, @team_2 => 0 }
     elsif calculation == 0
-      return { @team_1 => 1, @team_2 => 1 }
+      { @team_1 => 1, @team_2 => 1 }
     else
-      return { @team_1 => 0, @team_2 => 3 }
+      { @team_1 => 0, @team_2 => 3 }
     end
   end
 
   def teams_who_played
-    return @team_1, @team_2
+    [@team_1, @team_2]
   end
 end
